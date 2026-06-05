aa5d590944498c1ebf65a51eacbac24457cc40ee
Shravani-1325 <moreshravani93@gmail.com>
2026-06-05 14:05:25 +0530

    created flask app

---
+++ api/app.py
@@ -0,0 +1,117 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import joblib
+import json
+import random
+import os
+import sys #internal list of folder paths
+
+#>> Api directory path
+API_DIR = os.path.dirname(os.path.abspath(__file__))
+
+#>> Importing nlp file 
+sys.path.append(os.path.join(API_DIR, ".."))
+from nlp_processor import NLPProcessor, processor
+
+app = Flask(__name__)
+
+# -- Cors lets the chat.html talk to flask server without being blocked by the browser.
+
+CORS(app) # Enable cors for all routes 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+#>> Loading the Models 
+MODEL_PATH = os.path.join(API_DIR, "..", "models", "intent_classifier.pkl")
+VECTORIZER_PATH = os.path.join(API_DIR, "..", "models", "tfidf_vectorizer.pkl")
+INTENTS_PATH = os.path.join(API_DIR, "..", "data", "intents.json")
+
+print("Loading the models and vectorizer...")
+model = joblib.load(MODEL_PATH)
+tf_vectorizer = joblib.load(VECTORIZER_PATH)
+
+with open(INTENTS_PATH, "r") as f:
+    intents_data = json.load(f)
+    
+print("All files loaded successfully...")
+
+#>> Get Response by Intents Tags
+def get_response(intent_tag):
+    
+    for intent in intents_data["intents"]:
+        if intent["tag"] == intent_tag:
+            return random.choice(intent["responses"])
+        
+    return "I am sorry, I dindnt understand that"
+
+
+#>> Health Endpoint 
+@app.route("/health", methods = ["GET"])
+def health():
+    
+    #-- Use to verify the server is running before the testing/chat
+    
+    return jsonify({
+        "status": "ok",
+        "message" : "Chatbot API is running"
+    }), 200 # Success code
+
+#>> Main Chat End point
+@app.route("/chat", methods = ["POST"])
+def chat():
+    
+    # Getting the incomming json body via post in messagefield
+    data = request.get_json()
+    
+    # If empty message field
+    if not data:
+        return jsonify({
+            "error" : "Request body is empty. Send Json with a 'message' field",
+        }), 400 # Client side error
+        
+    message = data.get("message", '').strip()
+    
+    if not message:
+        return jsonify({
+            "error": "Message field is missing or empty"
+        }), 400
+    
+    #-- nlp preprocessor    
+    preprocessed = processor.main(message)
+    
+    #-- 500 words trained vectorizer
+    #-- Passing [preprocessed] turns our single string into a 1-row dataset list,
+    vector = tf_vectorizer.transform([preprocessed]) 
+    
+    #-- prediction intent
+    predicted_intent = model.predict(vector)[0]
+    
+    #--confidence score
+    confidence = float(model.predict_proba(vector).max())
+    
+    #-- If confidence is low
+    if confidence < 0.15:
+        return jsonify({
+            "response" : "I am not quite sure I understood that. Could you rephrase your question?",
+            "intent" : "uncertain",
+            "confidence" : round(confidence, 4)
+        }), 200
+    
+    #-- getting response frim intent.json
+    response_text = get_response(predicted_intent)
+    
+    # Returning the Json response
+    return jsonify({
+        "response": response_text,
+        "intent" : predicted_intent,
+        "confidence": round(confidence,4)
+    }), 200 # success code
+    
+#>> Running the Server
+
+# block only runs when directly execute app.py.
+if __name__ == "__main__":
+    app.run(debug=True, port =5000)
+    
+    
+    
